d9845733a3a39203379c133a3ee0b50979442b0b
HarikishorePalani <114153732+HarikishorePalani@users.noreply.github.com>
2023-07-15 15:49:47 +0530

    Add files via upload

---
+++ hotelapi/src/main/java/com/tnsdc/hotelapi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.tnsdc.hotelapi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HotelapiApplication.class);
+	}
+
+}
